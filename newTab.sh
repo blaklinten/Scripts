a84cd5f97ba8453faaddbfc0616cec6c5bd56b01
Lucas Nilsson <lucasnilsson92@gmail.com>
2018-11-26 13:07:08 +0100

    Update bookmarks functionality

--- newTab.sh
+++ newTab.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 Ans=$(sed -n '1~2!p' ~/.bookmarks | dmenu -i)
 
-if [[ $Ans == "AddBookmark" ]]
-then
-    echo $(echo "Input name of bookmark" | dmenu) >> ~/.bookmarks
-    echo $(echo "Input URL of bookmark" | dmenu) >> ~/.bookmarks
+if [[ $Ans == "AddBookmark" ]]; then
+    Name=$(echo "Input name of bookmark" | dmenu)
+    if [[ $Name != "" ]]; then
+        URL=$(echo "Input URL of bookmark" | dmenu)
+        if [[ $URL != "" ]]; then
+            echo $Name >> ~/testfile
+            echo $URL >> ~/testfile
+        fi
+    fi
     exit
 elif [[ $Ans == "Google" ]]
 then
-    exec firefox -new-tab http://www.google.com/search?q=$(echo "Input Google search" | dmenu | sed 's/ /+/g')
+    Search= $(echo "Input Google search" | dmenu | sed 's/ /+/g')
+    if [[ $Search != "" ]]; then
+        exec firefox -new-tab http://www.google.com/search?q=$Search
+    fi
 elif [[ $Ans != "" ]]
 then
-    if [[ $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks) == ""  ]]
-    then
+    if [[ $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks) == ""  ]]; then
         exec firefox -new-tab $Ans
     else
         exec firefox -new-tab $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks)
