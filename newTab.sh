f76c853d5004d9ab94e49d4dc0b1e6d65db52794
Lucas Nilsson <lucasnilsson92@gmail.com>
2018-11-26 11:04:43 +0100

    Update newTab script and added bookmark exampel syntax.

--- newTab.sh
+++ newTab.sh
@@ -1,6 +1,20 @@
-#!/bin/zsh
-read answer
-if [[ $answer != "" ]] then
-    exec firefox -new-tab $answer
-#else if [[ $answer == "?"    
+#!/bin/bash
+Ans=$(sed -n '1~2!p' ~/.bookmarks | dmenu -i)
+
+if [[ $Ans == "AddBookmark" ]]
+then
+    echo $(echo "Input name of bookmark" | dmenu) >> ~/.bookmarks
+    echo $(echo "Input URL of bookmark" | dmenu) >> ~/.bookmarks
+    exit
+elif [[ $Ans == "Google" ]]
+then
+    exec firefox -new-tab http://www.google.com/search?q=$(echo "Input Google search" | dmenu | sed 's/ /+/g')
+elif [[ $Ans != "" ]]
+then
+    if [[ $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks) == ""  ]]
+    then
+        exec firefox -new-tab $Ans
+    else
+        exec firefox -new-tab $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks)
+    fi
 fi
