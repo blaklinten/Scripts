6c08c4cd6f443fb692db3850689c1501e445aa75
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2018-12-01 15:10:07 +0100

    Added private browsing mode

--- firefox_open.sh
+++ firefox_open.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 Type=$1
+Session=-new-$Type
 Ans=$(sed -n '1~2!p' ~/.bookmarks | dmenu -i)
+
+if [[ $Ans == "Private" ]]
+then
+    Session=-private-window
+    Ans=$(sed -n '1~2!p' ~/.bookmarks | sed -n '/Private/!p' | sed -n '/AddBookmark/!p' |  dmenu -i)
+fi
 
 if [[ $Ans == "AddBookmark" ]]; then
     Name=$(echo "Input name of bookmark" | dmenu)
@@ -16,13 +23,13 @@ elif [[ $Ans == "Google" ]]
 then
     Search=$(echo "Input Google search" | dmenu | sed 's/ /+/g')
     if [[ $Search != "" ]]; then
-        exec firefox -new-$Type http://www.google.com/search?q=$Search
+        exec firefox $Session http://www.google.com/search?q=$Search
     fi
 elif [[ $Ans != "" ]]
 then
     if [[ $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks) == ""  ]]; then
-        exec firefox -new-$Type $Ans
+        exec firefox $Session $Ans
     else
-        exec firefox -new-$Type $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks)
+        exec firefox $Session $(sed -n "/^$Ans$/ {n;p}" ~/.bookmarks)
     fi
 fi
