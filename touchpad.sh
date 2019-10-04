f6bd9078502640305a156f461ebe8d96f7e60a2b
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2019-10-04 11:06:15 +0200

    Restore bookmarks + various fixes and added wpa_supplicant

--- touchpad.sh
+++ touchpad.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xinput set-prop AlpsPS/2 ALPS GlidePoint 285 1
-xinput set-prop AlpsPS/2 ALPS GlidePoint 293 1
+xinput set-prop "AlpsPS/2 ALPS GlidePoint" 298 1
+xinput set-prop "AlpsPS/2 ALPS GlidePoint" 306 1
