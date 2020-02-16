9efcb27ef45430fbe6009a2ba0c6c1665fe59c29
Lucas Nilsson <lucasnilsson92@gmail.com>
2020-02-16 20:16:01 +0100

    more sophisticated version, again

--- hdmi.sh
+++ hdmi.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+INTERNAL_SCREEN=$(xrandr|grep "primary"|awk '{print $1;}')
+EXTERNAL_SCREEN=HDMI1
 hdmi() {
-xrandr --output $1 --auto --output $2 --off
+xrandr --output $INTERNAL_SCREEN $1 $3 $4 --output $EXTERNAL_SCREEN $2 
 }
 
 case $1 in 
-  off) hdmi eDP1 HDMI1 ;; 
-  *) hdmi HDMI1 eDP1 ;;
+  off) hdmi --auto --off ;; 
+  "+") hdmi --auto --auto --right-of $EXTERNAL_SCREEN ;;
+  *) hdmi --off --auto ;;
 esac
