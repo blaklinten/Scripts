3a5949317d0df1e5874c00e40bc5ef1190b37614
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2020-11-25 09:04:16 +0100

    Automated update

--- stream.sh
+++ stream.sh
@@ -1,1 +1,1 @@
-cvlc -vvv v4l2:///dev/video0 --sout '#transcode{vcodec=theo,acodec=vorb,vb=400,ab=128,scale=0.5,channels=2,samplerate=44100,scodec=none}:http{mux=ogg,dst=:8080/} --sout-all --sout-keep'
+cvlc -vvv v4l2:///dev/video0 --sout '#transcode{vcodec=theo,acodec=vorb,vb=400,ab=128,scale=0.5,channels=2,samplerate=44100,scodec=none}:http{mux=ogg,dst=:8080/} --sout-all --sout-keep --sout-network-caching=1500'
