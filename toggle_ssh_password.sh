642f69d0c504fa4a7bb95242c409ff8f26151bb5
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-12-29 21:48:50 +0100

    A script to toggle password authentication for ssh.

---
+++ toggle_ssh_password.sh
@@ -0,0 +1,44 @@
+#/bin/sh
+
+[ "$UID" != "0" ] && echo "Must be run as root(uid=0)" && exit
+
+SSH_CONFIG="sshd_config"
+
+enable_unsafe_password_authenctication()
+{
+	echo "Enabling password"
+	mv "$SSH_CONFIG" "$SSH_CONFIG.safe"
+	mv "$SSH_CONFIG.unsafe" "$SSH_CONFIG"
+}
+
+disable_unsafe_password_authenctication()
+{
+	echo "Disabling password"
+	mv "$SSH_CONFIG" "$SSH_CONFIG.unsafe"
+	mv "$SSH_CONFIG.safe" "$SSH_CONFIG"
+}
+
+restart_sshd()
+{
+	echo "Restarting SSH Daemon"
+	systemctl restart sshd
+}
+
+cd /etc/ssh/
+
+case "$1" in
+	"on")
+		[ -f "$SSH_CONFIG.safe" ] && [ ! -f "$SSH_CONFIG.unsafe" ] && echo "Password already enabled, exiting..." && exit
+		enable_unsafe_password_authenctication
+		restart_sshd
+		;;
+	"off")
+		[ -f "$SSH_CONFIG.unsafe" ] && [ ! -f "$SSH_CONFIG.safe" ] && echo "Password already disabled, exiting..." && exit
+		disable_unsafe_password_authenctication
+		restart_sshd
+		;;
+	*)
+		echo "Usage: toggle_ssh_password.sh [on,off]"
+esac
+
+cd
