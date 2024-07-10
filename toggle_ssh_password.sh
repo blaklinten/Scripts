742f3c1c264429a5152149848820aaf0344d96cf
Lenny <lucas@fripost.org>
2024-07-10 11:06:24 +0200

    Automated update

--- toggle_ssh_password.sh
+++ toggle_ssh_password.sh
@@ -24,16 +24,23 @@ restart_sshd()
 
 cd /etc/ssh/ || exit
 
+if [ ! -f "$SSH_CONFIG.safe" ] && [ ! -f "$SSH_CONFIG.unsafe" ]; then
+  echo "Please set up a safe and unsafe version of sshd_config"
+  exit
+fi
+
 case "$1" in
 	"on")
 		[ -f "$SSH_CONFIG.safe" ] && [ ! -f "$SSH_CONFIG.unsafe" ] && echo "Password already enabled, exiting..." && exit
 		enable_unsafe_password_authenctication
 		restart_sshd
+    grep -m1 PasswordAuthentication "$SSH_CONFIG"
 		;;
 	"off")
 		[ -f "$SSH_CONFIG.unsafe" ] && [ ! -f "$SSH_CONFIG.safe" ] && echo "Password already disabled, exiting..." && exit
 		disable_unsafe_password_authenctication
 		restart_sshd
+    grep -m1 PasswordAuthentication "$SSH_CONFIG"
 		;;
 	*)
 		echo "Usage: toggle_ssh_password.sh [on,off]"
